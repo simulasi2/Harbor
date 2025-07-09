8efac6780bfe6bdc28f91b47a7f569564a14e593
simulasi2 <87004325+simulasi2@users.noreply.github.com>
2025-07-09 13:04:54 +0700

    Update harbor.sh

--- harbor.sh
+++ harbor.sh
@@ -6,12 +6,10 @@
 
 # Define the root directory to /home/container.
 # We can only write in /home/container and /tmp in the container.
-ROOTFS_DIR=/home/container
+ROOTFS_DIR=/home/container/unix
 
-# Define the Alpine Linux version we are going to be using.
-ALPINE_VERSION="3.18"
-ALPINE_FULL_VERSION="3.18.3"
-APK_TOOLS_VERSION="2.14.0-r2" # Make sure to update this too when updating Alpine Linux.
+# Define the Debian version we are going to be using.
+DEBIAN_VERSION="12"
 PROOT_VERSION="5.3.0" # Some releases do not have static builds attached.
 
 # Detect the machine architecture.
@@ -31,10 +29,11 @@ fi
 # Download & decompress the Alpine linux root file system if not already installed.
 if [ ! -e $ROOTFS_DIR/.installed ]; then
     # Download Alpine Linux root file system.
-    curl -Lo /tmp/rootfs.tar.gz \
-    "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/${ARCH}/alpine-minirootfs-${ALPINE_FULL_VERSION}-${ARCH}.tar.gz"
-    # Extract the Alpine Linux root file system.
-    tar -xzf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+    curl -Lo /tmp/rootfs.tar.xz \
+    "https://cloud.debian.org/images/cloud/bookworm/latest/debian-${DEBIAN_VERSION}-generic-${ARCH_ALT}.tar.xz"
+    # Extract the Debian root file system.
+    mkdir $ROOTFS_DIR
+    tar -xzf /tmp/rootfs.tar.xz -C $ROOTFS_DIR
 fi
 
 ################################
@@ -44,14 +43,10 @@ fi
 # Download static APK-Tools temporarily because minirootfs does not come with APK pre-installed.
 if [ ! -e $ROOTFS_DIR/.installed ]; then
     # Download the packages from their sources.
-    curl -Lo /tmp/apk-tools-static.apk "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/${ARCH}/apk-tools-static-${APK_TOOLS_VERSION}.apk"
     curl -Lo /tmp/gotty.tar.gz "https://github.com/sorenisanerd/gotty/releases/download/v1.5.0/gotty_v1.5.0_linux_${ARCH_ALT}.tar.gz"
     curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
     # Extract everything that needs to be extracted.
-    tar -xzf /tmp/apk-tools-static.apk -C /tmp/
     tar -xzf /tmp/gotty.tar.gz -C $ROOTFS_DIR/usr/local/bin
-    # Install base system packages using the static APK-Tools.
-    /tmp/sbin/apk.static -X "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/" -U --allow-untrusted --root $ROOTFS_DIR add alpine-base apk-tools
     # Make PRoot and GoTTY executable.
     chmod 755 $ROOTFS_DIR/usr/local/bin/proot $ROOTFS_DIR/usr/local/bin/gotty
 fi
@@ -68,32 +63,7 @@ fi
 
 # Print some useful information to the terminal before entering PRoot.
 # This is to introduce the user with the various Alpine Linux commands.
-clear && cat << EOF
-
- ██╗  ██╗ █████╗ ██████╗ ██████╗  ██████╗ ██████╗ 
- ██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
- ███████║███████║██████╔╝██████╔╝██║   ██║██████╔╝
- ██╔══██║██╔══██║██╔══██╗██╔══██╗██║   ██║██╔══██╗
- ██║  ██║██║  ██║██║  ██║██████╔╝╚██████╔╝██║  ██║
- ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
- 
- Welcome to Alpine Linux minirootfs!
- This is a lightweight and security-oriented Linux distribution that is perfect for running high-performance applications.
- 
- Here are some useful commands to get you started:
- 
-    apk add [package] : install a package
-    apk del [package] : remove a package
-    apk update : update the package index
-    apk upgrade : upgrade installed packages
-    apk search [keyword] : search for a package
-    apk info [package] : show information about a package
-    gotty -p [server-port] -w ash : share your terminal
- 
- If you run into any issues make sure to report them on GitHub!
- https://github.com/RealTriassic/Harbor
- 
-EOF
+clear
 
 ###########################
 # Start PRoot environment #
